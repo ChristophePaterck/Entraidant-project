b6a316b7d107acbb97d64b8240b15371538641a6
YohannPavone <yohann.pavone@oclock.school>
2024-02-21 10:23:16 +0100

    add

--- Entraidant/src/router.jsx
+++ Entraidant/src/router.jsx
@@ -1,14 +1,17 @@
 // Import de la fonction createBrowserRouter depuis la bibliothèque react-router-dom
-import { createBrowserRouter } from "react-router-dom";
-// Import de la fonction lazy depuis la bibliothèque react
-import { lazy } from "react";
-// Import des composants nécessaires depuis leurs emplacements respectifs
-import App from "./App/App.jsx";
-import ErrorPage from "./pages/ErrorPage/ErrorPage.jsx";
-import ServiceForm from'./pages/ServicesExchange/ServiceForm.jsx'
-
-
+// import { createBrowserRouter } from "react-router-dom";
+// // Import de la fonction lazy depuis la bibliothèque react
+// import { lazy } from "react";
+// // Import des composants nécessaires depuis leurs emplacements respectifs
 // import { rootLoader } from "./loaders/rootLoader.jsx";
+// import App from "./App/App.jsx";
+// import ErrorPage from "./pages/ErrorPage/ErrorPage.jsx";
+// import ServiceForm from "./pages/ServicesExchange/ServiceForm.jsx";
+// import Administratives from "./pages/Demarches/Demarches.jsx";
+// import DemarcheDetail from "./pages/Demarches/pages/Administratives/DemarcheDetail.jsx";
+// import AideDetail from "./pages/Demarches/pages/DemarchesFinancieres/AideDetail.jsx";
+// import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute.jsx";
+
 
 // Utilisation de lazy loading pour charger les composants de manière dynamique
 // Cela permet de ne pas charger tous les modules lors de la première connexion au site
@@ -35,18 +38,25 @@ const SignUp = lazy(() => import("./pages/SignUp/SignUp.jsx"));
 const SignIn = lazy(() => import("./pages/SignIn/SignIn.jsx"));
 // Composant de la page de profil utilisateur
 const Profile = lazy(() => import("./pages/Profile/Profile.jsx"));
+// const Administratives = lazy(() => import("./pages/Demarches/pages/Administratives/Administratives.jsx"));
+const DemarchesFinancieres = lazy(() =>
+  import(
+    "./pages/Demarches/pages/DemarchesFinancieres/DemarchesFinancieres.jsx"
+  )
+);
 
 //color
-//router permettant la navigation sur le site 
+//router permettant la navigation sur le site
 export const router = createBrowserRouter([
   {
     path: "/", // Chemin racine
     element: <App />, // Élément racine de l'application
+    loader: rootLoader,
     children: [
       // Enfants de l'élément racine
       {
         index: true, // Indique que cette route est la page d'accueil
-        errorElement: <ErrorPage />, // Composant à afficher en cas d'erreur
+        // errorElement: <ErrorPage />, // Composant à afficher en cas d'erreur
         element: <Homepage />, // Composant de la page d'accueil
       },
       {
@@ -61,14 +71,37 @@ export const router = createBrowserRouter([
         path: "/demarches", // Chemin vers la page des démarches
         element: <Demarches />, // Composant de la page des démarches
       },
-    
+      {
+        path: "/administratives",
+        element: <Administratives />,
+      },
+      {
+        path: "/demarche/:id",
+        element: <DemarcheDetail />,
+      },
+      {
+        path: "/aides",
+        element: <DemarchesFinancieres />,
+      },
+      {
+        path: "/aide/:id",
+        element: <AideDetail />,
+      },
       {
         path: "/services", // Chemin vers la page d'échange de services
-        element: <ServicesExchange />, // Composant de la page d'échange de services
+        element: (
+          <ProtectedRoute>
+            <ServicesExchange />
+          </ProtectedRoute>
+        ), // Composant de la page d'échange de services
       },
       {
         path: "/messagerie", // Chemin vers la page de messagerie
         element: <Messagerie />, // Composant de la page de messagerie
+      },
+      {
+        path: "/servicesform",
+        element: <ServiceForm />,
       },
       {
         path: "/signup", // Chemin vers la page d'inscription
@@ -79,16 +112,16 @@ export const router = createBrowserRouter([
         element: <SignIn />, // Composant de la page de connexion
       },
       {
-        path: "/profile", // Chemin vers la page de profil utilisateur
-        element: <Profile />, // Composant de la page de profil utilisateur
+        path: "/profil", // Chemin vers la page de profil utilisateur
+        element: (
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        ), // Composant de la page de profil utilisateur
       },
       {
-        path: "/category", // Chemin vers la page de profil utilisateur
-        element: < category />, // Composant de la page de profil utilisateur
-      },
-      {
-        path: "/services/:id", // Chemin vers la page de profil utilisateur
-        element: < ServiceForm />, // Composant de la page de profil utilisateur
+        path: "*", // Chemin vers la page de profil utilisateur
+        element: <ErrorPage />, // Composant de la page de profil utilisateur
       },
     ],
   },
